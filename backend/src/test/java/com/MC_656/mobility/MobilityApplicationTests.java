a4605aa886c9476a7d0bb07a7a39bbc2b887c976
Pucheranni <ra247149@ic.unicamp.br>
2025-06-12 17:52:26 -0300

    fix: Corrige erro de contexto no MobilityApplicationTests

--- backend/src/test/java/com/MC_656/mobility/MobilityApplicationTests.java
+++ backend/src/test/java/com/MC_656/mobility/MobilityApplicationTests.java
@@ -1,12 +1,14 @@
 package com.MC_656.mobility;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// A anotação foi modificada para excluir a configuração de banco de dados durante este teste
-@SpringBootTest(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class MobilityApplicationTests {
 
 	@Test
